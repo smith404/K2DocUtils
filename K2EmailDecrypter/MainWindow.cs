aa60fbee267be50974189bf610b5df9b5e638429
markg <markgrahamsmith6@gmail.com>
2021-12-24 08:31:48 +0100

    Added copyright

---
+++ K2EmailDecrypter/MainWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace K2EmailDecrypter
+{
+    public partial class MainWindow : Form
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public MainWindow()
+        {
+            log.Debug("Application started");
+            InitializeComponent();
+        }
+    }
+}
--- K2IMInterface/BaseObjects.cs
+++ K2IMInterface/BaseObjects.cs
@@ -1,4 +1,12 @@
-﻿using Newtonsoft.Json;
+﻿/*
+ * Copyright (c) 2021. K2-Software
+ * All software, both binary and source published by K2-Software (hereafter, Software)
+ * is copyrighted by the author (hereafter, K2-Software) and ownership of all right, 
+ * title and interest in and to the Software remains with K2-Software. By using or 
+ * copying the Software, User agrees to abide by the terms of this Agreement.
+ */
+
+using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System.Collections.Generic;
 using System.Reflection;
