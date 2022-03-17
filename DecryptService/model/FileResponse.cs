0470851aad8d9cea1b100208546f12fe719944b3
markg <markgrahamsmith6@gmail.com>
2022-03-17 13:24:40 +0100

    Added new project

---
+++ DecryptService/model/FileResponse.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DecryptService.model
+{
+    public class FileResponse
+    {
+        [JsonProperty(PropertyName = "filepath")]
+        public string FilePath { get; set; }
+
+        [JsonProperty(PropertyName = "sucess")]
+        public bool Sucess { get; set; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+    }
+}
